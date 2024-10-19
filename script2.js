c0b3fca4bd4580f993007c076f5f83eb93f44f75
Static-knowledge <151043413+Static-knowledge@users.noreply.github.com>
2024-10-19 14:04:39 +0530

    calculator
    modern new design calculator

---
+++ script2.js
@@ -0,0 +1,54 @@
+let display = document.getElementById('display');
+let currentInput = '';
+let operator = '';
+let previousInput = '';
+
+function appendNumber(number) {
+    currentInput += number;
+    display.innerText = `${previousInput} ${operator} ${currentInput}`;
+}
+
+function appendOperator(op) {
+    if (currentInput === '') return;
+    operator = op;
+    previousInput = currentInput;
+    currentInput = '';
+    display.innerText=`${previousInput} ${operator}`;
+}
+
+function clearDisplay() {
+    currentInput = '';
+    previousInput = '';
+    operator = '';
+    display.innerText = '0';
+}
+
+function deleteLast() {
+    currentInput = currentInput.slice(0, -1);
+    display.innerText = currentInput || '0';
+}
+
+function calculateResult() {
+    if (currentInput === '' || previousInput === '') return;
+    let result;
+    switch (operator) {
+        case '+':
+            result = parseFloat(previousInput) + parseFloat(currentInput);
+            break;
+        case '-':
+            result = parseFloat(previousInput) - parseFloat(currentInput);
+            break;
+        case '*':
+            result = parseFloat(previousInput) * parseFloat(currentInput);
+            break;
+        case '/':
+            result = parseFloat(previousInput) / parseFloat(currentInput);
+            break;
+        default:
+            return;
+    }
+    display.innerText = result;
+    currentInput = result.toString();
+    previousInput = '';
+    operator = ''; 
+}
